3ecf07b600ccfdf4fd84ff2595f072d054979aeb
Singh <703247488@genpact.com>
2019-09-03 15:23:32 +0530

    Change made

---
+++ Ramsey.java
@@ -0,0 +1,17 @@
+
+public class Ramsey {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float cpi = 30.55f;
+		int n = 100;
+		double tc = n*cpi*2;
+		System.out.println(tc);
+		
+		int a=2,b=3,c=4,d=5,e=7;
+		float result = (a+b+c+d+e)/5f;
+		System.out.println(result);
+		
+		
+	}
+}
